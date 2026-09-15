78c2f92e8799bdb4ca882283c0ea3bc794f7e866
CodelineAtyab <syed.atyab.hussain.om@gmail.com>
2026-09-15 18:27:17 +0400

    Committed existing Hello World Example

--- examples/hello_world_workflow.py
+++ examples/hello_world_workflow.py
@@ -1,13 +1,21 @@
 from typing import TypedDict
+from langgraph.graph import StateGraph
 
 
-class User(TypedDict):
-    id: int
-    name: str
-    email: str
-    active: bool
+class AgentState(TypedDict):
+    message: str
 
 
-user_record = User(id=1, name="Mr.A", email="mra@gmail.com", active=True);
-user_record["name"] = 1;
-print(user_record["name"]);
+def greet(shared_state: AgentState) -> AgentState:
+    return {"message": "Greetings! " + shared_state["message"]}
+
+
+graph_workflow = StateGraph(AgentState)
+graph_workflow.add_node("greet_node", greet)
+graph_workflow.set_entry_point("greet_node")
+graph_workflow.set_finish_point("greet_node")
+app = graph_workflow.compile()
+
+app.get_graph().print_ascii()
+
+print(app.invoke({"message": "Atyab"}))
